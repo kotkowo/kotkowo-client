e00f7854d0274a753b2029c0504297fdf521f8ab
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-01 15:41:37 +0100

    🎨 Cleanup project structure

---
+++ src/queries/get_cat.rs
@@ -0,0 +1,44 @@
+use crate::schema;
+
+#[derive(cynic::QueryVariables, Debug)]
+pub struct GetCatVariables<'a> {
+    pub id: &'a cynic::Id,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(graphql_type = "Query", variables = "GetCatVariables")]
+pub struct GetCat {
+    #[arguments(id: $id)]
+    pub cat: Option<CatEntityResponse>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct CatEntityResponse {
+    pub data: Option<CatEntity>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct CatEntity {
+    pub attributes: Option<Cat>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct Cat {
+    pub name: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cat_get() {
+        use cynic::QueryBuilder;
+        let id = &cynic::Id::new("test");
+        let operation = GetCat::build(
+            GetCatVariables{
+                id,
+        });
+        insta::assert_snapshot!(operation.query);
+    }
+}
