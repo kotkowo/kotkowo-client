1c873077a26e49786e6360daee082217a8e289ec
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-04-13 15:23:50 +0200

    :snowflake: devenv

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,15 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   dotenv.enable = true;
   languages.rust = {
     enable = true;
     channel = "nightly";
   };
-  packages = with pkgs; [openssl cargo-insta vscode-extensions.vadimcn.vscode-lldb.adapter];
+  packages = with pkgs; [
+    openssl
+    cargo-insta
+    vscode-extensions.vadimcn.vscode-lldb.adapter
+  ];
   enterShell = ''
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$DEVENV_PROFILE/lib/"
   '';
